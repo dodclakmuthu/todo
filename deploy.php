17e26c93175b8031eacdd1ef9a218b9df035d9a0
chamath lakmuthu <chamath@longwapps.com>
2024-04-18 23:15:56 +0530

    change deployer

---
+++ deploy.php
@@ -0,0 +1,22 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/dodclakmuthu/todo-laravel-api.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('dclakmuthu.xyz')
+    ->set('remote_user', 'ubuntu')
+    ->set('deploy_path', '/var/www/dclakmuthu.xyz');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
