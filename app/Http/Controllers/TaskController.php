0bc6d552c1a6ef6ca7ecbea37931e1ebd8675cfc
chamath lakmuthu <chamath@longwapps.com>
2024-08-31 11:26:38 +0530

    log

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\TaskRepository;
+use Log;
 
 class TaskController extends Controller
 {
   public function getTasks(Request $request)
   {
+    Log::debug('TaskController getTasks');
     $tasks = TaskRepository::getAllTasks();
     $data = ['success' => true,'tasks'=> $tasks];
     return response()->json($data, 200);
